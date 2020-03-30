517714d8ad57d75f4ed601d5209fbcdb16e3bc49
dayte <dayte.rodriguez@gmail.com>
2020-03-30 19:29:56 +0200

    Project created

---
+++ Fase1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
