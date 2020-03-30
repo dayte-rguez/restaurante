557c933d13cac0733275da735d7ed9fec31b2c74
dayte <dayte.rodriguez@gmail.com>
2020-03-30 19:39:39 +0200

    Creates: .- A variable per each bill up to 500 .- A variable to store the change .- 2 arrays, one for the menu (5 options) and other that will contain the price of each plate

--- Fase1/Program.cs
+++ Fase1/Program.cs
@@ -6,7 +6,19 @@ namespace Fase1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Create a variable per each bill up to 500 and one to store the change
+             2 arrays, one for the menu (5 options) and other that will 
+             contain the price of each plate*/
+            var b5     = 0;
+            var b10    = 0;
+            var b20    = 0;
+            var b50    = 0;
+            var b100   = 0;
+            var b200   = 0;
+            var b500   = 0;
+            var change = 0.0;
+            var menu   = new string[5];
+            var price  = new double[5];
         }
     }
 }
